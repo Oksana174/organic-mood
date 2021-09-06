e408106b60897d6f9b0dc1690b81cc7537c18a3e
Oksana <kisa-chel@mail.ru>
2021-09-06 16:45:42 +0500

    first commit

---
+++ build/js/script.js
@@ -0,0 +1,129 @@
+// menu gamburger
+
+let navMain = document.querySelector('.navigation');
+let button = document.querySelector('.header__menu-button');
+
+if (button) {
+    button.addEventListener('click', function (evt) {
+        evt.preventDefault ();
+        
+        document.body.classList.toggle('lock');
+        navMain.classList.toggle('navigation--open');
+        button.classList.toggle('header__menu-button--active');
+    });
+};
+
+//   прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.navigation__link[data-goto]');
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) 
+        {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto); 
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (navMain.classList.contains('navigation--open')) {
+                document.body.classList.remove('lock');
+                navMain.classList.remove('navigation--open');
+                button.classList.remove('header__menu-button--active');
+                navMain.classList.add('navigation--closed');
+            }
+
+            window.scrollTo ({
+               top: gotoBlockValue,
+               behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+};
+
+// слайдер для блока отзывы
+
+let btnRight = document.querySelector('.reviews__btn--next');
+let btnLeft = document.querySelector('.reviews__btn--prev');
+
+/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 0;
+
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    let sliderImages = document.querySelector('.reviews__list');
+    let slides = sliderImages.querySelectorAll('.reviews__item');
+    
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+  
+    for (let slide of slides) {
+        slide.classList.remove('reviews__item--active');
+    }
+    
+    slides[slideIndex - 1].classList.add('reviews__item--active');
+};
+
+btnRight.addEventListener('click', previousSlide);
+btnLeft.addEventListener('click', nextSlide);
+
+// слайдер для блока бестселлеры
+
+let btnNext = document.querySelector('.best-sellers__btn--next');
+let btnPrev = document.querySelector('.best-sellers__btn--prev');
+let slideIndexProduct = 0;
+
+showSlidesProduct(slideIndexProduct);
+
+function nextSlideProduct() {
+    showSlidesProduct(slideIndexProduct += 1);
+}
+
+function previousSlideProduct() {
+    showSlidesProduct(slideIndexProduct -= 1);  
+}
+
+/* Функция перелистывания: */
+function showSlidesProduct(n) {
+    let sliderImages = document.querySelector('.best-sellers__slider');
+    let slides = sliderImages.querySelectorAll('.product-card');
+    
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+        slideIndexProduct = 1;
+    }
+    if (n < 1) {
+        slideIndexProduct = slides.length;
+    }
+  
+    for (let slide of slides) {
+        slide.classList.remove('product-card--active');
+    }
+    
+    slides[slideIndexProduct - 1].classList.add('product-card--active');
+};
+
+btnPrev.addEventListener('click', previousSlideProduct);
+btnNext.addEventListener('click', nextSlideProduct);
---
+++ gulpfile.js
@@ -0,0 +1,173 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
+// const uglify = require("gulp-uglify");
+const terser = require("gulp-terser");
+
+
+const sassCss = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"));
+}
+exports.sassCss = sassCss;
+
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+exports.styles = styles;
+
+// html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build/"));
+}
+exports.html = html;
+
+// script
+
+// const scripts = () => {
+//   return gulp.src("source/js/**/*.js")
+//       .pipe(uglify())
+//       .pipe(rename("script.min.js"))
+//       .pipe(gulp.dest("build/js"));
+// }
+// exports.scripts = scripts;
+
+function scripts() {
+  return gulp.src("source/js/**/*.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"));
+}
+
+exports.scripts = scripts;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: "build"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html", gulp.series("html"));
+  gulp.watch("source/js/*.js", gulp.series("scripts"));
+}
+exports.default = gulp.series(
+  styles, server, watcher
+);
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 90, progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+}
+exports.images = images;
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+}
+exports.webp = createWebp;
+
+// svgsprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+}
+exports.sprite = sprite;
+
+// копирование
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/css/style.css"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+exports.copy = copy;
+
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
+
+// команды автоматизации
+
+const build = gulp.series(
+  clean,
+  sassCss,
+  copy,
+  styles,
+  html,
+  sprite,
+  scripts
+);
+exports.build = build;
+
+const start = gulp.series(
+  build,
+  server,
+  watcher
+);
+exports.start = start;
